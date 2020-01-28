897bc9e2e30d0f58f1f6bf464397c4cc30c1bf33
NeoDoggy <neodog0616@gmail.com>
2020-01-28 20:48:23 +0800

    update 2020/01/28 20:48

--- bot.js
+++ bot.js
@@ -8,8 +8,21 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '/adduser') {
-    msg.reply('好滴\n正在呼喚管理員\n老公~~有人在叫你啦');
+    msg.reply('はい!!\n正在呼叫管理員!!');
   }
+  if (msg.content === '/dick' || msg.content === '/hentai') {
+    msg.reply('はい!!\nI love dicks\n**--該用戶已被禁聲--**');
+  }
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === '新進人員');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`${member}!歡迎你來到T22群組\n\`\`\`md\n#貼心提醒\n[進入伺服要做的][請先查看以下頻道]\n1. <info>\n2. <注意事項>\n就醬掰\`\`\``);
 });
 
 

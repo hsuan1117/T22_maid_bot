c08a7582e945ce6a19ee319669346d0b880f35cf
NeoDoggy <neodog0616@gmail.com>
2020-01-29 17:17:32 +0800

    update 2020/01/29 17:17 [stopped backstage & fixing]

--- bot.js
+++ bot.js
@@ -29,11 +29,11 @@ client.on('message', msg => {
     msg.channel.send("変態！！", {files:["./image/hentai.png"]});
   }
 
-  //backstage
+  /*backstage
   if(msg.channel!=='伺服器後台'){
 	console.log(msg.author.username + "-sent: [" + msg + "]\nuserID: " + msg.author.id) ;
 	channel.send(msg.author.username + "-sent: [" + msg + "]\nuserID: " + msg.author.id) ;
-  }
+  }*/
 });
 
 
